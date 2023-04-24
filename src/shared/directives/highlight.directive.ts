beac29e4472d2f9943d8030c3606277b131bee63
emrebican <emrebican11@hotmail.com>
2023-04-24 11:17:34 +0300

    item hover is darker now

--- src/shared/directives/highlight.directive.ts
+++ src/shared/directives/highlight.directive.ts
@@ -1,13 +1,18 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import {
+  Directive,
+  HostBinding,
+  HostListener
+} from '@angular/core';
 
 @Directive({ selector: '[appHighlight]' })
 export class HighlightDirective {
-  @HostBinding('class') defaultClass = 'shadow-lg brightness-100';
+  @HostBinding('class') defaultClass =
+    'shadow-lg brightness-100';
 
   constructor() {}
 
   @HostListener('mouseenter') mouseEnter(eventData: Event) {
-    this.defaultClass = 'shadow-2xl brightness-125';
+    this.defaultClass = 'shadow-xl brightness-105';
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event) {
